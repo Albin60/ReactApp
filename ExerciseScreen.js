614ae191d8a5c9b9b0b68a6af0e0430174d4b9be
Albin60 <albin.menxhiqi34@gmail.com>
2024-10-02 17:44:44 +0200

    React App
    React App

---
+++ ExerciseScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Text, StyleSheet, View} from 'react-native';
+
+
+const ExerciseScreen = () => {
+    let message = 'Hi theree!';
+
+    message = 'Hi there from Albin Menxhiqi';
+    return(
+        <text>{message}</text>
+    )
+}
+
+export default ExerciseScreen;
---
+++ TestingApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text, StyleSheet, View} from 'react-native';
+
+
+
+const TestingApp = ()=> {
+ const Mesazhi = "Ky eshte testim "
+
+  return(
+    <Text>{Mesazhi}</Text>
+  )
+}
+
+
+export default TestingApp;
